34b3a0a4b67f03820d3d3c80b53da5cea23dfb49
Przemyslaw Satro <przemyslaw.satro@gmail.com>
2025-06-22 00:18:04 +0200

    change: builder to swc; add: config prettier, config env

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`\x1b[46m \x1b[35m app is running on PORT: ${process.env.PORT ?? 3000} \x1b[0m`);
 }
 bootstrap();
