131ca272b464adbf884c909d0ed2060e814b4a08
Przemyslaw Satro <przemyslaw.satro@gmail.com>
2025-06-22 11:50:04 +0200

    add abortOnError:false option to NestApp; create profile module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ProfileModule,
   ],
   controllers: [AppController],
   providers: [AppService],
