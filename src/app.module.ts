b66b7a28673b6da9d70ea95bea31be3905ecc538
Przemyslaw Satro <przemyslaw.satro@gmail.com>
2025-10-12 22:18:05 +0200

    add envs, database and database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ProfileModule } from './profile/profile.module';
+import { DatabaseModule } from './database/database.module';
+import config from './config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      load: config,
     }),
+    DatabaseModule,
     ProfileModule,
   ],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -6,4 +6,6 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
   console.log(`\x1b[46m \x1b[35m app is running on PORT: ${process.env.PORT ?? 3000} \x1b[0m`);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('\x1b[31m App failed to start:\x1b[0m', err);
+});
